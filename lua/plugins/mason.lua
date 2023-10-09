71f7a8dbfc8d7e98e098355f3088538d9513184c
itapai <ksem.eks@gmail.com>
2023-10-09 08:36:36 +0200

    updates

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,17 +6,17 @@ return {
       vim.list_extend(opts.ensure_installed, {
         ensure_installed = {
           -- 2: LSP
-          "ansible-language-server",
+          -- "ansible-language-server",
           "bash-language-server",
           "dockerfile-language-server",
-          "helm-ls",
+          -- "helm-ls",
           "html-lsp",
           "lua-language-server",
           "spectral-language-server",
-          "terraform-ls",
+          -- "terraform-ls",
 
           -- 4: Linter
-          "ansible-lint",
+          -- "ansible-lint",
           "shellcheck",
           "yamllint",
 
