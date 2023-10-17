e1b49379b39ece821317695552248dd11f662643
itapai <ksem.eks@gmail.com>
2023-10-17 10:47:50 +0200

    update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,26 +1,16 @@
 return {
   {
     "williamboman/mason.nvim",
-
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         ensure_installed = {
-          -- 2: LSP
-          -- "ansible-language-server",
           "bash-language-server",
           "dockerfile-language-server",
-          -- "helm-ls",
           "html-lsp",
           "lua-language-server",
           "spectral-language-server",
-          -- "terraform-ls",
-
-          -- 4: Linter
-          -- "ansible-lint",
           "shellcheck",
           "yamllint",
-
-          -- 5: Formatter
           "jq",
           "stylua",
           "shfmt",
